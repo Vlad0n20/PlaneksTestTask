2c8a4e2006c8ad2e37c0728977213d11321deb65
Vlad0n23 <vlad.onischuk1234@gmail.com>
2024-01-19 20:58:14 +0200

    Add serializers for User model

---
+++ apps/user/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+
+from .models import User
+
+
+class UserListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username']
+
+
+class UserDitailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'first_name', 'last_name', 'is_active', 'email']
+
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        user = User.objects.create(**validated_data)
+        user.set_password(validated_data.get('password'))
+        return user
+
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
