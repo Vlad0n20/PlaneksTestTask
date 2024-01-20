607687428a6cffc5d5471260fd470079f3ad8639
Vlad0n23 <vlad.onischuk1234@gmail.com>
2024-01-20 20:31:30 +0200

    Create ColumnViewSet

--- apps/column/views.py
+++ apps/column/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from apps.viewsets import CustomModelViewSet
 
-# Create your views here.
+from .models import Column
+from .serializers import ColumnDitailSerializer, ColumnCreateSerializer, ColumnListSerializer
+
+
+class ColumnViewSet(CustomModelViewSet):
+    queryset = Column.objects.all()
+    serializer_class = ColumnDitailSerializer
+
+    def get_serializer_class(self):
+        if self.action == ['create', 'update', 'partial_update']:
+            return ColumnCreateSerializer
+        elif self.action == 'retrieve':
+            return ColumnDitailSerializer
+        else:
+            return self.serializer_class
