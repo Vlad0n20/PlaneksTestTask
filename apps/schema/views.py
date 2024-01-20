15d856bbf509ffad8d6547476eed42cd3b03a133
Vlad0n23 <vlad.onischuk1234@gmail.com>
2024-01-20 20:21:08 +0200

    Create SchemaViewSet

--- apps/schema/views.py
+++ apps/schema/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from apps.viewsets import CustomModelViewSet
 
-# Create your views here.
+from .models import Schema
+from .serializers import SchemaListSerializer, SchemaDitailSerializer
+
+
+class SchemaViewSet(CustomModelViewSet):
+    queryset = Schema.objects.all()
+    serializer_class = SchemaListSerializer
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return SchemaDitailSerializer
+        else:
+            return self.serializer_class
