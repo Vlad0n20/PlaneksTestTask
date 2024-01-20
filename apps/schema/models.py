6c793dc2ee5430d0d7a4bfca74b9588621a39ef2
Vlad0n23 <vlad.onischuk1234@gmail.com>
2024-01-20 20:20:50 +0200

    Add schema models

--- apps/schema/models.py
+++ apps/schema/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from apps.user.models import User
+
+
+class Schema(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='schemas')
+    name = models.CharField('Name', max_length=150)
+    number_of_records = models.SmallIntegerField('Number of records', default=1)
+
+    def __str__(self):
+        return f'Schema {self.name}'
+
+    class Meta:
+        verbose_name = 'Schema'
+        verbose_name_plural = 'Schemas'
+        ordering = ['name']
